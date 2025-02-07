3c2401bad3acb5f60729203163879ce99c222cae
Pham Cao Hoang An <pchoangan@gmail.com>
2025-02-07 09:51:07 +0700

    Update pull-model.sh

--- ollama/pull-model.sh
+++ ollama/pull-model.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
+./bin/ollama serve & # Khởi động Ollama server
+pid=$!
 echo "Pulling llama3.2:3b model"
 ollama pull llama3.2:3b
 
 sleep 5 # Chờ 5 giây
 
-./bin/ollama serve & # Khởi động Ollama server
+wait $pid
